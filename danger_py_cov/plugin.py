1800d1b4cd0eff33cfba43ae9587abeb4f3294ac
Jakub Turek <jkbturek@gmail.com>
2020-02-04 14:56:15 +0100

    Move code generation report to report.py

--- danger_py_cov/plugin.py
+++ danger_py_cov/plugin.py
@@ -1,46 +1,13 @@
-from typing import List, Optional
+from typing import Optional
 
 from danger_python.plugins import DangerPlugin
 
-from danger_py_cov.models import CoverageFile
-from danger_py_cov.report import CoverageReport
+from danger_py_cov.report import CoverageReport, generate_report
 
 
 class DangerCoverage(DangerPlugin):
     def report_coverage(self, report_path: str, sources_path: Optional[str] = None):
         report = CoverageReport(report_path, source=sources_path)
-        report_entries = [entry_for_file(report, f) for f in report.files()]
-        generated_report = generate_report(report_entries)
 
-        for markdown in generated_report:
+        for markdown in generate_report(report):
             self.markdown(markdown)
-
-
-def entry_for_file(report: CoverageReport, filename: str) -> CoverageFile:
-    return CoverageFile(
-        name=filename,
-        total_statements=report.total_statements(filename),
-        missed_statements=len(report.missed_statements(filename)),
-        total_branches=report.total_branches(filename),
-        missed_branches=report.missed_branches(filename),
-    )
-
-
-def generate_report(files: List[CoverageFile]) -> List[str]:
-    coverage = f"{total_coverage(files):.2f}%"
-    return [f"### Current coverage is `{coverage}`"]
-
-
-def total_coverage(files: List[CoverageFile]) -> float:
-    statements = (
-        "total_statements",
-        "total_branches",
-        "missed_statements",
-        "missed_branches",
-    )
-
-    numbers = list(map(lambda s: sum(map(lambda f: getattr(f, s), files)), statements))
-    hits = numbers[0] + numbers[1] - numbers[2] - numbers[3]
-    total = numbers[0] + numbers[1]
-
-    return float(hits) * 100.0 / float(total)
--- danger_py_cov/report.py
+++ danger_py_cov/report.py
@@ -1,7 +1,9 @@
 import xml.etree.ElementTree as ET
-from typing import Iterator, Optional, Tuple
+from typing import Iterator, List, Optional, Tuple
 
 from pycobertura import Cobertura
+
+from .models import CoverageFile
 
 
 class CoverageReport(Cobertura):
@@ -24,3 +26,38 @@ class CoverageReport(Cobertura):
         coverage = line.attrib["condition-coverage"]
         covered_branches = coverage.split("(")[-1][:-1].split("/")
         return (int(covered_branches[0]), int(covered_branches[1]))
+
+
+def generate_report(report: CoverageReport) -> List[str]:
+    report_entries = [entry_for_file(report, f) for f in report.files()]
+    return generate_report_for_files(report_entries)
+
+
+def entry_for_file(report: CoverageReport, filename: str) -> CoverageFile:
+    return CoverageFile(
+        name=filename,
+        total_statements=report.total_statements(filename),
+        missed_statements=len(report.missed_statements(filename)),
+        total_branches=report.total_branches(filename),
+        missed_branches=report.missed_branches(filename),
+    )
+
+
+def generate_report_for_files(files: List[CoverageFile]) -> List[str]:
+    coverage = f"{total_coverage(files):.2f}%"
+    return [f"### Current coverage is `{coverage}`"]
+
+
+def total_coverage(files: List[CoverageFile]) -> float:
+    statements = (
+        "total_statements",
+        "total_branches",
+        "missed_statements",
+        "missed_branches",
+    )
+
+    numbers = list(map(lambda s: sum(map(lambda f: getattr(f, s), files)), statements))
+    hits = numbers[0] + numbers[1] - numbers[2] - numbers[3]
+    total = numbers[0] + numbers[1]
+
+    return float(hits) * 100.0 / float(total)
