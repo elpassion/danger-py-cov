62b548a20f4bf506eb230ab9482f1dbc6f5b1376
Jakub Turek <jkbturek@gmail.com>
2020-02-06 15:02:57 +0100

    Fix dangerfile.py

--- dangerfile.py
+++ dangerfile.py
@@ -2,7 +2,7 @@ import danger_py_cov
 
 danger_py_cov.report_coverage("cov.xml", minimum_coverage=100.0)
 
-has_changelog = danger.git.modified_files.includes("CHANGELOG.md")
+has_changelog = "CHANGELOG.md" in danger.git.modified_files
 
 if not has_changelog:
-    warn("Please add the changelog entro to CHANGELOG.md file")
+    warn("Please add the changelog entry to CHANGELOG.md file")
