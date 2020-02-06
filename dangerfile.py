8cf565b555763bd017784cade084214e6d406486
Jakub Turek <jkbturek@gmail.com>
2020-02-06 14:55:46 +0100

    Check the presence of the changelog entry

--- dangerfile.py
+++ dangerfile.py
@@ -1,3 +1,8 @@
 import danger_py_cov
 
 danger_py_cov.report_coverage("cov.xml", minimum_coverage=100.0)
+
+has_changelog = danger.git.modified_files.includes("CHANGELOG.md")
+
+if not has_changelog:
+    warn("Please add the changelog entro to CHANGELOG.md file")
